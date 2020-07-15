c6e953d012c177d37ea216f21f56bfc28e41877c
vieru <mixerandr3w001@gmail.com>
2020-07-15 20:43:06 +0300

    am pus clasele astea in fisiere separate

--- req.dart
+++ req.dart
@@ -1,42 +1,6 @@
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
-/// o clasa pentru un response de la glot cu un snippet simplu
-class GlotSnippetSimple {
-  /// ex: "fp3r3oqtq8"
-  final String id;
-
-  GlotSnippetSimple(this.id);
-
-  GlotSnippetSimple.fromJson(Map<String, dynamic> json) : id = json['id'];
-}
-
-/// pentru proprietatea [files] din [GlotSnippet]
-class GlotSnippetFile {
-  final String content;
-
-  GlotSnippetFile(this.content);
-
-  GlotSnippetFile.fromJson(Map<String, dynamic> json)
-      : content = json['content'];
-}
-
-/// clasa pentru snippetul glot full
-class GlotSnippet {
-  /// ex: "fp3r3oqtq8"
-  final String id;
-
-  final List<GlotSnippetFile> files;
-
-  GlotSnippet(this.id, this.files);
-
-  GlotSnippet.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        files = (json['files'] as List)
-            .map((i) => GlotSnippetFile.fromJson(i))
-            .toList();
-}
-
 /// Listeaza snippeturile
 ///
 /// https://github.com/prasmussen/glot-snippets/blob/master/api_docs/list_snippets.md
