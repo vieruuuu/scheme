3fd10b363287737a93684b5a6d36f8e461645eca
vieru <mixerandr3w001@gmail.com>
2020-07-15 11:39:40 +0300

    am creat clasa GlotSnippetSimple si func GlotListSnippets, mai am de lucru la ele

--- req.dart
+++ req.dart
@@ -1,20 +1,85 @@
-// curl --request GET \
-//     --url 'https://snippets.glot.io/snippets'
-
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
-void main() async {
-  // This example uses the Google Books API to search for books about http.
-  // https://developers.google.com/books/docs/overview
+/// o clasa pentru un response de la glot cu un snippet simplu \
+/// trebuie sa comentez niste proprietati ca sa fac codul mai eficient
+class GlotSnippetSimple {
+  /// ex: "https://snippets.glot.io/snippets/fp3r3oqtq8"
+  final String url;
+
+  /// ex: "fp3r3oqtq8"
+  final String id;
+
+  /// ex: "2015-04-23T22:03:41Z"
+  final String created;
+
+  /// ex: "2015-04-23T22:03:41Z"
+  final String modified;
+
+  /// ex: "perl"
+  final String language;
+
+  /// ex: "Hello world"
+  final String title;
+
+  /// ex: true
+  final bool public;
+
+  /// ex: "anonymous"
+  final String owner;
+
+  /// ex: "20f74f4277cafb12cd46e68008574dc92c5be35e"
+  final String files_hash;
+
+  GlotSnippetSimple(this.url, this.id, this.created, this.modified,
+      this.language, this.title, this.public, this.owner, this.files_hash);
+
+  GlotSnippetSimple.fromJson(Map<String, dynamic> json)
+      : url = json['url'],
+        id = json['id'],
+        created = json['created'],
+        modified = json['modified'],
+        language = json['language'],
+        title = json['title'],
+        public = json['public'],
+        owner = json['owner'],
+        files_hash = json['files_hash'];
+
+  Map<String, dynamic> toJson() => {
+        'url': url,
+        'id': id,
+        'created': created,
+        'modified': modified,
+        'language': language,
+        'title': title,
+        'public': public,
+        'owner': owner,
+        'files_hash': files_hash,
+      };
+}
+
+/// Listeaza snippeturile \
+/// curl --request GET \
+///     --url 'https://snippets.glot.io/snippets'
+void GlotListSnippets() async {
   final String url = 'https://snippets.glot.io/snippets';
 
-  // Await the http get response, then decode the json-formatted response.
-  var response = await http.get(url);
+  final http.Response response = await http.get(url);
+
   if (response.statusCode == 200) {
-    var jsonResponse = convert.jsonDecode(response.body);
-    print(jsonResponse[0]['url']);
+    final List<dynamic> jsonResponse = convert.jsonDecode(response.body);
+
+    // mai eficient asa, daca opresc forul undeva celelalte nu s deserializate
+    for (final Map<String, dynamic> res in jsonResponse) {
+      final GlotSnippetSimple snippet = GlotSnippetSimple.fromJson(res);
+
+      // momentan le afisez ca nush ce sa fac cu ele
+      print(snippet.id);
+    }
   } else {
+    // trebuie sa gasesc alta modalitate pentru a ma ocupa de erori
     print('error: ${response.statusCode}.');
   }
 }
+
+void main() async {}
